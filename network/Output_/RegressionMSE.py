ec7ce22349bfedc144f91725a13d38cf261dff9c
LilPomidorLil <rozhinthe@ya.ru>
2022-04-20 21:11:51 +0300

    Добавлен выход с ошибкой регрессии MSE

---
+++ network/Output_/RegressionMSE.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from network.Output import Output
+
+from numpy import ndarray as ARRAY
+
+class RegressionMSE(Output):
+    def evaluate(self, prev_layer_data: ARRAY, target: ARRAY):
+        nrows, ncols = prev_layer_data.shape
+
+        if (target.shape[0] != nrows) or (target.shape[1] != ncols):
+            raise IndexError(f"Target dimension is not last hidden layer dimension")
+
+        self.__m_din = prev_layer_data - target
+
+
+    def backprop_data(self):
+        return self.__m_din
+
+    def loss(self):
+        return 0.5 * np.square(self.__m_din).sum()
+
+    
