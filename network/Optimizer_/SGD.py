a914ff41a3d5c6968fe1494603a44c884274e168
LilPomidorLil <rozhinthe@ya.ru>
2022-04-20 21:03:31 +0300

    Добавлен стохастический гд

---
+++ network/Optimizer_/SGD.py
@@ -0,0 +1,22 @@
+from network.OptimiZer import Optimizer
+
+from numpy import ndarray as ARRAY
+
+class SGD(Optimizer):
+    """
+    Оптимайзер
+
+    Стохастический градиентный спуск
+    """
+    def __init__(self, lrate: float, decay: float):
+        """
+        Инициализация класса
+        :param lrate: длина шага
+        :param decay: поправочный коэффицент
+        """
+        self.lrate = lrate
+        self.decay = decay
+
+    def update(self, dvec: ARRAY, vec: ARRAY):
+        vec -= self.lrate * (dvec + self.decay * vec)
+        return vec
