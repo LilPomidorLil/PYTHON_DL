993938442942e2912cfcf76176a0c2af59a0757a
LilPomidorLil <rozhinthe@ya.ru>
2022-04-20 16:21:15 +0300

    Добавлен базовый класс слоя и оптимайзера

---
+++ network/Layer.py
@@ -0,0 +1,71 @@
+from numpy import ndarray as ARRAY
+from OptimiZer import Optimizer
+
+class Layer:
+    """
+    Базовый класс для всех слоев
+    """
+
+    def __init__(self, in_size: int, out_size: int):
+        self.__m_in_size = in_size
+        self.__m_out_size = out_size
+
+    def in_size(self):
+        return self.__m_in_size
+
+    def out_size(self):
+        return self.__m_out_size
+
+    def init(self, mu: float, sigma: float):
+        """
+        Инициализация слоя
+
+        :param mu: мат.ожидание
+        :param sigma: дисперсия
+        :return: None
+        """
+        pass
+
+    def forward(self, prev_layer_data: ARRAY):
+        """
+        Проход вперед внутри одного слоя
+        :param prev_layer_data: - данные нейронов предыдущего слоя
+        :return: None
+        """
+        pass
+
+    def output(self):
+        """
+        :return: Значения нейронов в слою после функции активации
+        """
+        pass
+
+    def backprop(self, prev_layer_data: ARRAY, next_layer_data: ARRAY):
+        """
+        Реализация метода обратного распространения ошибки.
+
+        :param prev_layer_data: значения нейронов предыдущего слоя,
+	    которые также являются входными значениями этого слоя.
+
+        :param next_layer_data: значения нейронов следующего слоя,
+        которые также являются выходными значениями этого слоя
+
+        :return:
+        """
+        pass
+
+    def backprop_data(self):
+        """
+        :return: next_layer_data в Layer.backprop()
+        """
+        pass
+
+    def update(self, opt: Optimizer):
+        """
+        Обновление весов сетки
+        :param opt: - объект класса Optimizer
+        :return:
+        """
+        pass
+
+
---
+++ network/Optimizer.py
@@ -0,0 +1,21 @@
+from numpy import ndarray as ARRAY
+
+class Optimizer:
+    """
+    Базовый класс, реализация оптимайзеров сетки
+    """
+
+    def reset(self):
+        """
+        :return: Сброс параметров модели
+        """
+        pass
+
+    def update(self, dvec: ARRAY, vec: ARRAY):
+        """
+        Обновление весов сетки
+        :param dvec: производная весов
+        :param vec: веса
+        :return:
+        """
+        pass
